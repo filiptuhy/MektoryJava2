78c9b0e2dc0a265d4dd6d96ffa938cc4f4e447c2
filiptuhy <filip.tuhy@gmail.com>
2015-11-01 10:54:30 -0800

    division by zero, not done

--- Module1/UnitTesting/src/ee/ttu/demo/Calculator.java
+++ Module1/UnitTesting/src/ee/ttu/demo/Calculator.java
@@ -19,4 +19,11 @@ public class Calculator {
 		return num1 - num2;
 	}
 
+	public void div(int i, int divisor) {
+		if(divisor == 0){
+			throw new IllegalArgumentException("Argument 'divisor' is 0");
+		}
+		
+	}
+
 }
--- Module1/UnitTesting/src/ee/ttu/demo/CalculatorTest.java
+++ Module1/UnitTesting/src/ee/ttu/demo/CalculatorTest.java
@@ -2,13 +2,21 @@ package ee.ttu.demo;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class CalculatorTest {
+import ee.ttu.solution.SimpleTesting;
 
+public class CalculatorTest {
+	Calculator calc ;
+
+	@Before
+	public void setUp(){
+		 calc = new Calculator();
+	}
+	
 	@Test
 	public void testAdd() {
-		Calculator calc = new Calculator();
 		int num1 = 5;
 		int num2 = 10;
 		
@@ -19,14 +27,20 @@ public class CalculatorTest {
 	
 	@Test
 	public void testSub(){
-		Calculator calc = new Calculator();
 		int num1 = 5;
 		int num2 = 10;
 		
 		int result = calc.sub(num1, num2);
 		int correctAnswer = -5;
 		assertEquals(correctAnswer, result);
-
+	}
+	
+	@Test(expected = Exception.class)  
+	public void testDivisionByZero(){
+		int num1 = 10;
+		int zero = 0;
+		
+		calc.div(num1,zero);
 	}
 
 }
